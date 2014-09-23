163c7457a5b5dbd47e39e8037ac3d4cc2ccda47b
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-09-23 15:38:10 +0200

    First version of button toggle

--- client/app/questions/questions.js
+++ client/app/questions/questions.js
@@ -13,4 +13,24 @@ angular.module('nocheInv.questions', ['ngRoute'])
   $http.get('questions/questions.json').success(function(data) {
     $scope.questions = data;
   });
+
+  $scope.selectExample = function(example) {
+    example.selected = ! example.selected;
+  };
+
+  $scope.isSelected = function(item) {
+    if (item.examples !== undefined) {
+      // question
+      var selected = false;
+
+      angular.forEach(item.examples, function(example) {
+        selected = selected || example.selected;
+      });
+
+      return selected;
+    } else {
+      // example
+      return item.selected;
+    }
+  };
 }]);
