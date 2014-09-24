f205c2e515cafe22407cc41a4c479dbc4c7278c7
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-09-24 13:01:26 +0200

    Finishing result scaffold

--- client/app/questions/results.js
+++ client/app/questions/results.js
@@ -3,5 +3,6 @@
 angular.module('nocheInv.results', ['ngRoute'])
 
 .controller('ResultsCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.achievements = [ "home", "edit" ];
 }]);
              
