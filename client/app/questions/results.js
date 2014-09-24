f116407f7d250a001feeccf774b3a8e91a331071
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-09-24 17:44:02 +0200

    Fix icon position

--- client/app/questions/results.js
+++ client/app/questions/results.js
@@ -3,7 +3,7 @@
 angular.module('nocheInv.results', ['ngRoute'])
 
 .controller('ResultsCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
-  $scope.achievements = [ "home", "edit" ];
+  $scope.achievements = [ "home", "edit", "car", "code-fork" ];
 
   $scope.finish = function() {
     $location.path('/finish');
