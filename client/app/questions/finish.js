171d1fcfa09842ad1c011a44ec0cf5cf04071a5c
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-09-25 18:24:00 +0200

    Clear finish timeout

--- client/app/questions/finish.js
+++ client/app/questions/finish.js
@@ -2,11 +2,6 @@
 
 angular.module('nocheInv.finish', ['ngRoute'])
 
-.controller('FinishCtrl', ['$scope', '$http', '$location', '$timeout', function($scope, $http, $location, $timeout) {
-  $scope.finish = function() {
-    $timeout(function() {
-      $location.path('/');
-    }, 10000);
-  };
+.controller('FinishCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
 }]);
              
