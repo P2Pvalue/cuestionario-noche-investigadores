f97934a19c273304d421965acfa9f1dadf1875d8
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2014-09-25 15:38:56 +0200

    Revert "ng-class bug fixed"
    This reverts commit 813f99436636cd913910865b73740f9fe229e113.

--- client/app/questions/results.js
+++ client/app/questions/results.js
@@ -3,10 +3,7 @@
 angular.module('nocheInv.results', ['ngRoute'])
 
 .controller('ResultsCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
-  $scope.achievements = 
-    [ 
-      "home", "edit", "car", "code-fork", "globe", "gears", "exchange", "money", "newspaper-o", "lemon-o"
-    ];
+  $scope.achievements = [ "home", "edit", "car", "code-fork" ];
 
   $scope.finish = function() {
     $location.path('/finish');
