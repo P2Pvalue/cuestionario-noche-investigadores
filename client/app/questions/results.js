813f99436636cd913910865b73740f9fe229e113
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2014-09-25 15:35:28 +0200

    ng-class bug fixed

--- client/app/questions/results.js
+++ client/app/questions/results.js
@@ -3,7 +3,10 @@
 angular.module('nocheInv.results', ['ngRoute'])
 
 .controller('ResultsCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
-  $scope.achievements = [ "home", "edit", "car", "code-fork" ];
+  $scope.achievements = 
+    [ 
+      "home", "edit", "car", "code-fork", "globe", "gears", "exchange", "money", "newspaper-o", "lemon-o"
+    ];
 
   $scope.finish = function() {
     $location.path('/finish');
